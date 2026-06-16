f45ab99b9d6af3b5504516f25f044be5d2b5d57b
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2026-06-16 15:41:38 -0300

    feat: add sentry to track errors and performance

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://171892af33c23d5ea1cadd4d5b737b6c@o4511576517967872.ingest.us.sentry.io/4511576518885376",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
